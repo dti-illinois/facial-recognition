2a28d33e5c25c094b72cfa1c1f04d7bb1f12fa9f
Abhinav Tekulapally <abhitekula@users.noreply.github.com>
2020-04-16 02:31:08 -0400

    Added sessions on server side and on client side only send next frame after previous face recognition response was received

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template      
-from flask import request
+from flask import Flask, render_template
+from flask import request, session
 import sys
 import threading
 import cv2
@@ -7,61 +7,62 @@ import boto3
 import requests
 import base64
 import numpy as np
+import os
 
 app = Flask(__name__, static_url_path='', static_folder='static/picojs')
+app.secret_key = str(os.urandom(16))
+rekognition = boto3.client("rekognition", "us-west-2")
+
 
 @app.route('/')
 def main_page():
+    session["is_processing"] = False
     return render_template("index.html")
 
-person = 'None'
-@app.route('/person', methods=['POST'])
-def process_person():
-    global person
-    person = request.data.decode("utf-8")
-    # sys.stderr.write(person + '\n\n')
-    return person
 
-@app.route('/person', methods=['GET'])
-def return_person():
-    return person
+@app.route('/people_detected', methods=['POST'])
+def process_people_detected():
+    people_detected = request.data.decode("utf-8")
+    session['people_detected'] = people_detected
+    # sys.stderr.write(people_detected + '\n\n')
+    return people_detected
 
-rekognition = boto3.client("rekognition", "us-west-2")
-processing = False
 
-def process_image(data):
-    global person
-    global processing
-    encoded_data = data[22:]
-    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    output = ""
-    response = rekognition.search_faces_by_image(
-        Image={
-            "Bytes": bytearray(cv2.imencode('.png', img)[1])
-        },
-        CollectionId="arc-face-rec-test",
-        FaceMatchThreshold=80,
-    )
+@app.route('/people_detected', methods=['GET'])
+def return_people_detected():
+    return session.get('people_detected', "None")
 
-    for face in response['FaceMatches']:
-        face = response['FaceMatches'][0]['Face']
-        output += "Face Detected: " + face['ExternalImageId'] + ", Confidence: " + str(face['Confidence']) + '<br>'
-    person = output
-    # sys.stderr.write(output + '\n\n')
-    processing = False
-    return output
 
 @app.route('/detect', methods=['POST'])
 def detect_faces():
-    global processing
-    if not processing:
-        processing=True
-        # print("Starting recognizeFace thread")
-        th = threading.Thread(target=process_image, args=[request.get_data()])
-        th.start()
-    return "Success"
+    # Get raw request data, remove header, and convert to image
+    encoded_data = request.get_data()[22:]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Send image to AWS Rekognition and process result
+    output = ""
+    try:
+        response = rekognition.search_faces_by_image(
+            Image={
+                "Bytes": bytearray(cv2.imencode('.png', img)[1])
+            },
+            CollectionId="arc-face-rec-test",
+            FaceMatchThreshold=80,
+        )
+
+        for resp in response['FaceMatches']:
+            face = resp['Face']
+            output += "Face Detected: " + \
+                face['ExternalImageId'] + ", Confidence: " + \
+                str(face['Confidence']) + '<br>'
+    except rekognition.exceptions.InvalidParameterException:
+        output = "None"
+
+    # sys.stderr.write(output + '\n\n')
+    session['people_detected'] = output
+    return output
+
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=False, host="0.0.0.0")
+    app.run(debug=True, threaded=True, host="0.0.0.0")
