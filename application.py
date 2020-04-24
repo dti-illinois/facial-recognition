46b4907fa76111e127a9f81895d4003bcc485934
Abhinav Tekulapally <abhitekula@users.noreply.github.com>
2020-04-24 19:15:39 -0400

    Added initial login support

--- application.py
+++ application.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, flash
 import boto3
 import base64
 import os
+from application.forms import *
+from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
 
 application = Flask(__name__, static_url_path='')
 application.secret_key = str(os.urandom(16))
 rekognition = boto3.client("rekognition", "us-west-2")
-
+admin_pass = os.environ.get('FACIAL_RECOGNITION_ADMIN_PASS', "default")
+login_manager = LoginManager()
+login_manager.init_app(application)
 
 @application.route('/')
 def main_page():
     return render_template("index.html")
 
 
-@application.route('/login')
-def login_page():
-    return render_template("login.html")
+@application.route('/add_face')
+@login_required
+def add_face_page():
+    return render_template("add_face.html")
 
 
 @application.route('/detect', methods=['POST'])
@@ -45,6 +50,40 @@ def detect_faces():
 
     return output
 
+class User(UserMixin):
+    def get_id(self):
+        return "admin"
+
+AdminUser = User()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return AdminUser
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('Must be logged in first.')
+    return login_page()
+
+@application.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        if form.username.data == "admin" and form.password.data == admin_pass:
+            login_user(AdminUser)
+            return redirect(('/'))
+        else:
+            flash('Invalid username or password.')
+            return redirect(('/login'))
+    return render_template("login.html", form=form)
+
+@application.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0")
